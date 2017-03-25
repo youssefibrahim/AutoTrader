06f168426f1fa858f58fed1f8df382a5b904d158
Youssef Ibrahim <youssef.ibrahim22@gmail.com>
2017-03-25 19:36:01 -0400

    added landing page and search page... search page still needs correct fields instead of textarea

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
+@app.route('/signUp')
+def signUp():
+	return render_template('signup.html')
+
+@app.route('/search')
+def search():
+	return render_template('search.html')
+
+@app.route('/result', methods=["POST"])
+def display_search():
+	return render_template('result.html')
+
+@app.route('/logIn')
+def logIn():
+	return render_template('login.html')
+ 
 if __name__ == '__main__':
 	app.run()
